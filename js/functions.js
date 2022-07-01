69179fba40d369465cd03c368c28f5d88c82d753
disection <diseccionmexico@gmail.com>
2022-07-01 11:26:34 -0500

    first time

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+/**
+ * Ejercicio 
+ * Dada una lista de 10 usuarios 
+ * [ nombre, edad, profesion]
+ * 1. mostrar todos los usuarios
+ */
+
+// user list
+let users = [
+    { nombre:'Sandra',   edad:32,   profesion:'mamá' },
+    { nombre:'Rafael',   edad:22,   profesion:'contador' },
+    { nombre:'Arturo',   edad:31,   profesion:'pedagógo' },
+    { nombre:'Alfredo',  edad:30,   profesion:'albañil' },
+    { nombre:'Roman',    edad:29,   profesion:'electricista' },
+    { nombre:'Sandra',   edad:20,   profesion:'pastelero' },
+    { nombre:'Emiliano', edad:29,   profesion:'carpintero' },
+    { nombre:'Ramón',    edad:23,   profesion:'panadero' },
+    { nombre:'Saul',     edad:39,   profesion:'maestro' },
+    { nombre:'Mario',    edad:29,   profesion:'diseñador' }
+]
+console.log(users)
